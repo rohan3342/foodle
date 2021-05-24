d868ac49cc050ab2dc8e938e9b820765c7c758d7
Rohan Kumar Singh <rosingh3342@gmail.com>
2021-05-24 23:56:53 +0530

    MapCalloutComponent & CompactRestaurantInfoComponent created

--- src/features/map/screens/map-screen.js
+++ src/features/map/screens/map-screen.js
@@ -5,6 +5,7 @@ import { LocationContext } from "../../../services/location/location-context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants-context";
 
 import { Search } from "../components/search-component";
+import { MapCallout } from "../components/map-callout-component";
 
 const Map = styled(MapView)`
   height: 100%;
@@ -43,7 +44,11 @@ export const MapScreen = () => {
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
               }}
-            />
+            >
+              <MapView.Callout>
+                <MapCallout restaurant={restaurant} />
+              </MapView.Callout>
+            </MapView.Marker>
           );
         })}
       </Map>
