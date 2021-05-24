655f586f2694f03e438c95335fa0148834174669
Rohan Kumar Singh <rosingh3342@gmail.com>
2021-05-24 17:47:40 +0530

    Created MapScreen, Created Map-SearchComponent & Optimize the Restaurant-SearchComponent

--- src/features/map/components/search-component.js
+++ src/features/map/components/search-component.js
@@ -1,10 +1,15 @@
 import React, { useContext, useState, useEffect } from "react";
-import { Searchbar } from "react-native-paper";
 import styled from "styled-components/native";
+import { Searchbar } from "react-native-paper";
+
 import { LocationContext } from "../../../services/location/location-context";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
+  position: absolute;
+  z-index: 999;
+  top: 40px;
+  width: 100%;
 `;
 
 export const Search = () => {
@@ -16,6 +21,7 @@ export const Search = () => {
     <SearchContainer>
       <Searchbar
         placeholder="Search for a location"
+        icon="map"
         value={searchKeyword}
         onSubmitEditing={() => search(searchKeyword)}
         onChangeText={(text) => setSearchKeyword(text)}
--- src/framework/navigation/app-navigation.js
+++ src/framework/navigation/app-navigation.js
@@ -5,6 +5,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import { SafeArea } from "../../components/utility/safe-area-component";
 import { RestaurantsNavigator } from "./restaurants-navigator";
+import { MapScreen } from "../../features/map/screens/map-screen";
 
 const Tab = createBottomTabNavigator();
 
@@ -17,12 +18,6 @@ const Tab_Icon = {
 const Settings = () => (
   <SafeArea>
     <Text>Settings</Text>
-  </SafeArea>
-);
-
-const Map = () => (
-  <SafeArea>
-    <Text>Map</Text>
   </SafeArea>
 );
 
@@ -46,7 +41,7 @@ export const AppNavigator = () => {
         }}
       >
         <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-        <Tab.Screen name="Map" component={Map} />
+        <Tab.Screen name="Map" component={MapScreen} />
         <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
