108507935892d84af17cac65bf56fb66cc8da1bf
Rohan Kumar Singh <rosingh3342@gmail.com>
2021-05-25 14:03:32 +0530

    AsyncStorage Added to FavouritesContext & Optimized the CompactRestaurantInfoComponent

--- src/components/restaurant/compact-restaurant-info-component.js
+++ src/components/restaurant/compact-restaurant-info-component.js
@@ -26,8 +26,8 @@ const View = styled.View`
 
 const isAndroid = Platform.OS === "android";
 
-export const CompactRestaurantInfo = ({ restaurant }) => {
-  const Image = isAndroid ? CompactWebview : CompactImage;
+export const CompactRestaurantInfo = ({ restaurant, isMap }) => {
+  const Image = isAndroid && isMap ? CompactWebview : CompactImage;
 
   return (
     <View>
--- src/services/favourites/favourites-context.js
+++ src/services/favourites/favourites-context.js
@@ -1,16 +1,47 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+
   const add = (restaurant) => setFavourites([...favourites, restaurant]);
+
   const remove = (restaurant) => {
     const newFavourites = favourites.filter(
       (x) => x.placeId !== restaurant.placeId
     );
     setFavourites(newFavourites);
   };
+
+  const saveFavourites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@favourites", jsonValue);
+    } catch (e) {
+      console.error("[AsyncStorage] STORING: ", e);
+    }
+  };
+
+  const loadFavourites = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@favourites");
+      if (value !== null) {
+        setFavourites(JSON.parse(value));
+      }
+    } catch (e) {
+      console.error("[AsyncStorage] LOADING: ", e);
+    }
+  };
+
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
 
   return (
     <FavouritesContext.Provider
